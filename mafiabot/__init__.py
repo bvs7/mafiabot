ec991970c8a623d73db59d14080c93e93b3799cf
Brian Scaramella <bvs7@cornell.edu>
2022-06-01 22:27:31 -0700

    Clean up old code

--- mafiabot/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import mafiastate
-from . import mafiactrl
-from . import chatinterface
-from .test import test_util
-from .util import VEnum, VEnumMeta, auto
-from .resp_lib import resp_lib, get_resp
-from .MSave import MSaveEncoder, msave, mload, mafia_hook
--- mafiabot/chatinterface/__init__.py
+++
@@ -1,3 +0,0 @@
-from .MChat import MChat, MDM, CastError
-from .MServer import MServer, TestMServer, ACCESS_KW, MCmd
-from .TestMChat import TestMChat, TestMDM, TestMChatError
