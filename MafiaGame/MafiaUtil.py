1be63d07877047ee9d1795f4efdb5f59cc037fe1
bvs7 <bvs7@cornell.edu>
2023-01-05 17:22:44 -0700

    Remove leftover python

--- MafiaGame/MafiaUtil.py
+++
@@ -1,5 +0,0 @@
-
-
-class MafiaFormatError(Exception):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
--- archive/mafiactrl/__init__.py
+++
@@ -1,4 +0,0 @@
-from .MController import MController
-from .MGame import MGame
-from .MTimer import MTimer, FastMTimer
-from .MLobby import MLobby
