e217098e0eff445f3c8ef9f4d84efb14b479e3c2
Brian Scaramella <bvs7@cornell.edu>
2025-01-18 01:48:28 -0700

    New skeletons for Game and State

--- src/engine/game/mod.rs
+++ src/engine/game/mod.rs
@@ -213,3 +213,229 @@ impl Game {
         }
     }
 }
+#[cfg(test)]
+mod tests {
+    use std::{sync::Arc, time::Duration};
+
+    use anyhow::Result;
+
+    use tokio::{
+        sync::{
+            oneshot, Mutex, MutexGuard, OwnedMutexGuard, RwLock, RwLockWriteGuard, TryLockError,
+        },
+        task::JoinHandle,
+    };
+
+    use crate::engine::{
+        interface::{Action, ActionRx, ActionTx, Error, EventRx, EventTx},
+        state::{phase::Phase, players::Players, role::Role, rules::Rules, Choice},
+    };
+
+    struct Game {
+        state: Arc<RwLock<State>>,
+
+        action_tx: ActionTx,             // Allow cloning
+        action_rx: Arc<Mutex<ActionRx>>, // Option so we can take it
+        event_tx: EventTx,               // Allow subscribing
+
+                                         // Timers... could be in state?
+    }
+
+    impl Game {
+        // How can state be created without roles? Should rolegen be rolled in?
+        pub fn new(users: Vec<u64>, rules: Rules) -> Self {
+            todo!()
+        }
+
+        pub fn new_with_roles(users: Vec<u64>, roles: Vec<u64>, rules: Rules) -> Self {
+            todo!()
+        }
+
+        pub fn load(state: State) -> Self {
+            todo!()
+        }
+
+        pub fn action_tx(&self) -> ActionTx {
+            self.action_tx.clone()
+        }
+        pub fn event_rx(&self) -> EventRx {
+            self.event_tx.subscribe()
+        }
+
+        // If this returns a result, make sure it returns self as well?
+        // Consumes self and returns an Arc<Self>, meaning no more &mut calls...
+        pub fn start(self) {
+            // Start action handler?
+            todo!()
+        }
+
+        pub async fn run(self) -> Result<(), TryLockError> {
+            let action_rx = self.action_rx.try_lock_owned()?;
+            State::action_handler(self.state, action_rx).await;
+            Ok(())
+        }
+    }
+
+    struct State {
+        id: u64,
+        day: u32,
+        phase: Phase,
+        players: Players,
+        rules: Rules,
+        tx: EventTx,
+        grace_period_timer: Option<JoinHandle<()>>,
+    }
+
+    impl State {
+        pub fn new(id: u64, registry: Vec<(u64, Role)>, rules: Rules) -> Self {
+            todo!()
+        }
+
+        // Should validate action be done first?
+        pub fn validate_action(&self, action: Action) -> Result<(), Error> {
+            todo!()
+        }
+
+        pub async fn action_handler(
+            this: Arc<RwLock<Self>>,
+            mut action_rx: OwnedMutexGuard<ActionRx>,
+        ) {
+            while let Some((action, resp)) = action_rx.recv().await {
+                Self::handle_action(&this, action, resp);
+            }
+        }
+        // The game pointer is used for... Event tx, to send events...
+        // And for timers to have a handle on the state mutex...
+        // It would be nice to do this differently...
+        pub async fn handle_action(
+            this: &Arc<RwLock<Self>>,
+            action: Action,
+            resp: oneshot::Sender<Result<(), Error>>,
+        ) -> anyhow::Result<()> {
+            // TODO: Check if locking read, validating, then on Ok locking write and re-validating would work?
+            let mut wstate = this.write().await;
+            // TODO: should this return anything else?
+            let result = wstate.validate_action(action);
+            resp.send(result.clone());
+
+            enum ActionResult {
+                Election { choice: Choice, hammer: u64 },
+                Dawn,
+                None,
+            }
+
+            // HANDLE ACTIONS
+
+            let result = match action {
+                Action::Vote { voter, ballot } => {
+                    // wstate.vote -> Result<Option<(Choice,usize)>>
+                    // Ok(Some(Election))
+                    let reached_new_election = Some(Some(1));
+                    let hammer = voter;
+
+                    if let Some(choice) = reached_new_election {
+                        ActionResult::Election { choice, hammer }
+                    } else {
+                        ActionResult::None
+                    }
+                }
+
+                Action::Target { actor, choice } => {
+                    // ...
+                    let night_done = true;
+                    if night_done {
+                        ActionResult::Dawn
+                    } else {
+                        ActionResult::None
+                    }
+                }
+                Action::Scheme { killer, mark } => {
+                    // ...
+                    let night_done = true;
+                    if night_done {
+                        ActionResult::Dawn
+                    } else {
+                        ActionResult::None
+                    }
+                }
+                _ => ActionResult::None,
+            };
+
+            // IF DAWN OR ELECTION IS DETECTED
+            match result {
+                ActionResult::Election { choice, hammer } => {
+                    let h = tokio::spawn(Self::election_timer(this.clone(), choice, hammer));
+                    if let Some(old_h) = wstate.grace_period_timer.replace(h) {
+                        old_h.abort();
+                    }
+                }
+                ActionResult::Dawn => {
+                    let h = tokio::spawn(Self::dawn_timer(this.clone()));
+                    if let Some(old_h) = wstate.grace_period_timer.replace(h) {
+                        old_h.abort();
+                    }
+                }
+                ActionResult::None => {}
+            }
+
+            drop(wstate);
+
+            todo!()
+        }
+
+        fn check_election(&self, choice: Choice) -> Result<Option<(Vec<u64>)>> {
+            todo!()
+        }
+
+        fn election(&mut self, choice: Choice, hammer: u64, voters: Vec<u64>) -> Result<()> {
+            todo!()
+        }
+
+        fn dawn(&mut self) -> Result<()> {
+            todo!()
+        }
+
+        async fn election_timer(this: Arc<RwLock<Self>>, choice: Choice, hammer: u64) {
+            tokio::time::sleep(Duration::from_secs(10)).await;
+            let mut wstate = this.write().await;
+            // TODO: Error tracing
+            if let Some(users) = wstate.check_election(choice).unwrap() {
+                wstate.election(choice, hammer, users).unwrap();
+            }
+            wstate.grace_period_timer = None;
+        }
+        async fn dawn_timer(this: Arc<RwLock<Self>>) {
+            tokio::time::sleep(Duration::from_secs(10)).await;
+            let mut wstate = this.write().await;
+            // TODO: Error tracing
+            wstate.dawn().unwrap();
+            wstate.grace_period_timer = None;
+        }
+
+        // Why does state need a mutex? Because status, action handler, and timers might contest
+        // Should state know about the mutex? Yeah, maybe... It could take itself at every entry
+        // Including timers.
+        // event broadcast could be done locally... we could even just have it as a function?
+        // Hmmm, maybe. Self.tx(event) is a Fn(Event) -> ()
+    }
+
+    #[test]
+    #[ignore]
+    fn typical_use() {
+        // Create game with... rules? Players? Roles?
+        // Assigned roles could come with rules
+        // From a loaded state
+
+        // Id comes from a file, which is then incremented... Don't worry about concurrency for now...
+        let users = vec![];
+        let rules = Rules {};
+        // let roles = vec![];
+        let game: Game = Game::new(users, rules); // Roles are generated... at start
+                                                  // let game: Game = Game::new_with_roles(users, roles, rules);
+
+        // let game: Game = Game::load(state); // require details of chats, etc to resume?
+
+        // let game
+        // Should there be some kind of "resume" functionality? Can we assume from_state can handle that?
+    }
+}
